d4afd35079c26b67335a79e8587fcf22a131a16e
racelo <kraef@protonmail.com>
2024-07-07 15:42:43 +0800

    Create a function that gets user input and returns it

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-let playerSelection = "";
-let computerSelection = "";
+// let playerSelection = "";
+// let computerSelection = "";
 
+// Get computer choice
 function getComputerChoice() {
   let randomChoice = Math.floor(Math.random() * 3) + 1;
   switch (randomChoice) {
@@ -13,65 +14,78 @@ function getComputerChoice() {
   }
 }
 
-function playRound(playerSelection, computerSelection) {
-  playerSelection = prompt("rock, paper, scissors???").toLowerCase();
-  computerSelection = getComputerChoice();
+// console.log(getComputerChoice());
 
-  if (playerSelection === "rock" && computerSelection === "paper") {
-    return "You lose! Paper beats rock!";
-  } else if (playerSelection === "paper" && computerSelection === "rock") {
-    return "You win! Nice pick!";
-  } else if (playerSelection === "scissors" && computerSelection === "rock") {
-    return "You lose! Rock beats scissors!";
-  } else if (playerSelection === "rock" && computerSelection === "scissors") {
-    return "You win! Nice pick!";
-  } else if (playerSelection === "paper" && computerSelection === "scissors") {
-    return "You lose! Scissors beats paper!";
-  } else if (playerSelection === "scissors" && computerSelection === "paper") {
-    return "You win! Nice pick!";
-  } else if (playerSelection === computerSelection) {
-    return "It's a tie!";
-  } else {
+function getHumanChoice(choice) {
+  choice = prompt("rock, paper, scissors???");
+  if (!choice) {
     return "Invalid input";
-  }
-}
-
-function playGame() {
-  // let i = 0;
-  let userScore = 0;
-  let computerScore = 0;
-
-  // while (i < 5) {
-  //   let playRoundResult = playRound(playerSelection, computerSelection);
-  //   console.log(playRoundResult);
-
-  //   if (/win/.test(playRoundResult)) {
-  //     userScore += 1;
-  //   } else if (/lose/.test(playRoundResult)) {
-  //     computerScore += 1;
-  //   }
-  //   ++i;
-  // }
-
-  for (i = 0; i < 5; i++) {
-    let playRoundResult = playRound(playerSelection, computerSelection);
-    console.log(playRoundResult);
-
-    if (/win/.test(playRoundResult)) {
-      userScore += 1;
-    } else if (/lose/.test(playRoundResult)) {
-      computerScore += 1;
-    }
-  }
-
-  if (userScore > computerScore) {
-    return `Congrats you win! You got ${userScore} correct answers!`;
-  } else if (userScore === computerScore) {
-    return "draw!";
-    return computerScore;
   } else {
-    return `Computer got ${computerScore} correct answers. Try again next time!`;
+    return choice.toLowerCase();
   }
 }
 
-console.log(playGame());
+// console.log(getHumanChoice());
+
+// function playRound(playerSelection, computerSelection) {
+//   playerSelection = prompt("rock, paper, scissors???").toLowerCase();
+//   computerSelection = getComputerChoice();
+
+//   if (playerSelection === "rock" && computerSelection === "paper") {
+//     return "You lose! Paper beats rock!";
+//   } else if (playerSelection === "paper" && computerSelection === "rock") {
+//     return "You win! Nice pick!";
+//   } else if (playerSelection === "scissors" && computerSelection === "rock") {
+//     return "You lose! Rock beats scissors!";
+//   } else if (playerSelection === "rock" && computerSelection === "scissors") {
+//     return "You win! Nice pick!";
+//   } else if (playerSelection === "paper" && computerSelection === "scissors") {
+//     return "You lose! Scissors beats paper!";
+//   } else if (playerSelection === "scissors" && computerSelection === "paper") {
+//     return "You win! Nice pick!";
+//   } else if (playerSelection === computerSelection) {
+//     return "It's a tie!";
+//   } else {
+//     return "Invalid input";
+//   }
+// }
+
+// function playGame() {
+//   // let i = 0;
+//   let userScore = 0;
+//   let computerScore = 0;
+
+//   // while (i < 5) {
+//   //   let playRoundResult = playRound(playerSelection, computerSelection);
+//   //   console.log(playRoundResult);
+
+//   //   if (/win/.test(playRoundResult)) {
+//   //     userScore += 1;
+//   //   } else if (/lose/.test(playRoundResult)) {
+//   //     computerScore += 1;
+//   //   }
+//   //   ++i;
+//   // }
+
+//   for (i = 0; i < 5; i++) {
+//     let playRoundResult = playRound(playerSelection, computerSelection);
+//     console.log(playRoundResult);
+
+//     if (/win/.test(playRoundResult)) {
+//       userScore += 1;
+//     } else if (/lose/.test(playRoundResult)) {
+//       computerScore += 1;
+//     }
+//   }
+
+//   if (userScore > computerScore) {
+//     return `Congrats you win! You got ${userScore} correct answers!`;
+//   } else if (userScore === computerScore) {
+//     return "draw!";
+//     return computerScore;
+//   } else {
+//     return `Computer got ${computerScore} correct answers. Try again next time!`;
+//   }
+// }
+
+// console.log(playGame());
